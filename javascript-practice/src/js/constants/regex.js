59ee29ba2504bc22dd0e8e5617f83f348658097a
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-05-04 17:37:29 +0700

    Update file change

---
+++ javascript-practice/src/js/constants/regex.js
@@ -0,0 +1,4 @@
+export const REGEX = {
+  //Must enter 6 characters including capital letters and special characters
+  PASSWORD: /^(?=.*[A-Z])(?=.*[!@#$%^&*]).{6,}$/,
+}
--- javascript-practice/src/js/views/toast.js
+++ javascript-practice/src/js/views/toast.js
@@ -1,14 +1,14 @@
 const showToast = (message, type = 'success') => {
-    const toastElement = document.getElementById('toast-message');
+  const toastElement = document.getElementById('toast-message')
 
-    if (!toastElement) return; 
-    toastElement.textContent = message;
-    const toastClass = (type === 'error') ? 'toast-success' : 'toast-error';
+  if (!toastElement) return
+  toastElement.textContent = message
+  const toastClass = type === 'error' ? 'toast-success' : 'toast-error'
 
-    toastElement.className = `toast-message ${toastClass} show`;
-    setTimeout(() => {
-        toastElement.classList.remove('show');
-    }, 3000);
-};
+  toastElement.className = `toast-message ${toastClass} show`
+  setTimeout(() => {
+    toastElement.classList.remove('show')
+  }, 3000)
+}
 
-export default showToast;
+export default showToast
