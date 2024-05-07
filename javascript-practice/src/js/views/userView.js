e37fad8ae1c92ddb1273c07854592a2d1430f9de
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-05-07 08:45:00 +0700

    Create Header Dashboard UI

--- javascript-practice/src/js/views/userView.js
+++ javascript-practice/src/js/views/userView.js
@@ -1,59 +1,77 @@
-import { validatePassword } from '../helpers/validation';
-import { ERROR_MESSAGE } from '../constants/message';
+import { validatePassword } from '../helpers/validation'
+import { ERROR_MESSAGE } from '../constants/message'
 
 export const {
   REQUIRED_FIELD_EMAIL,
   REQUIRED_FIELD_PASSWORD,
   INVALID_EMAIL,
   INVALID_PASSWORD,
-} = ERROR_MESSAGE;
+} = ERROR_MESSAGE
 
 export default class UserView {
   constructor() {
-    this.loginFormEl = document.getElementById('login-form');
-    this.emailFormEl = document.getElementById('email');
-    this.passwordFormEl = document.getElementById('password');
-    this.emailErrorEl = document.getElementById('email-error');
-    this.passwordErrorEl = document.getElementById('password-error');
+    this.loginFormEl = document.getElementById('login-form')
+    this.emailFormEl = document.getElementById('email')
+    this.passwordFormEl = document.getElementById('password')
+    this.emailErrorEl = document.getElementById('email-error')
+    this.passwordErrorEl = document.getElementById('password-error')
+    this.arrowEl = document.getElementById('arrow')
+    this.logoutEl = document.getElementById('logout')
+
+    this.isArrowUp = true
+    this.binDropDow()
   }
 
   bindFormLogin = (submitLogin) => {
     this.loginFormEl.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const valueEmail = this.emailFormEl.value;
-      const valuePassword = this.passwordFormEl.value;
+      e.preventDefault()
+      const valueEmail = this.emailFormEl.value
+      const valuePassword = this.passwordFormEl.value
 
-      this.emailErrorEl.textContent = '';
-      this.passwordErrorEl.textContent = '';
+      this.emailErrorEl.textContent = ''
+      this.passwordErrorEl.textContent = ''
 
       if (!valueEmail) {
-        this.emailErrorEl.textContent = `${REQUIRED_FIELD_EMAIL}`;
+        this.emailErrorEl.textContent = `${REQUIRED_FIELD_EMAIL}`
       }
 
       if (!valuePassword) {
-        this.passwordErrorEl.textContent = `${REQUIRED_FIELD_PASSWORD}`;
+        this.passwordErrorEl.textContent = `${REQUIRED_FIELD_PASSWORD}`
 
-        return;
+        return
       }
 
       if (!validatePassword(valuePassword)) {
-        this.passwordErrorEl.textContent = `${INVALID_PASSWORD}`;
+        this.passwordErrorEl.textContent = `${INVALID_PASSWORD}`
 
-        return;
+        return
       }
 
-      submitLogin(valueEmail, valuePassword);
+      submitLogin(valueEmail, valuePassword)
     })
 
     this.emailFormEl.addEventListener('input', () => {
-      this.emailErrorEl.textContent = '';
+      this.emailErrorEl.textContent = ''
     })
     this.passwordFormEl.addEventListener('input', () => {
-      this.passwordErrorEl.textContent = '';
+      this.passwordErrorEl.textContent = ''
     })
-  };
+  }
+
+  binDropDow = () => {
+    this.arrowEl.addEventListener('click', () => {
+      if (this.isArrowUp) {
+        // this.arrowEl.style.transform = 'rotate(180deg)'
+        this.logoutEl.style.display = 'block'
+      } else {
+        // this.arrowEl.style.transform = 'rotate(0deg)'
+        this.logoutEl.style.display = 'none'
+      }
+      this.isArrowUp = !this.isArrowUp
+    })
+  }
 
   redirectPage = (page) => {
-    window.location.replace(page);
-  };
-};
+    window.location.replace(page)
+  }
+}
