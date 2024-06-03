89283b7aaf96037ac1144403b812b746864c122e
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-06-03 09:20:41 +0700

    clear function controller on app.js

--- javascript-practice/src/js/app.js
+++ javascript-practice/src/js/app.js
@@ -4,30 +4,18 @@ import UserModel from './models/userModel';
 import DashboardController from './controllers/dashboardController';
 import DashboardView from './views/dashboardView';
 import { PATHS } from './constants/urls';
-import { LOCAL_STORAGE } from './constants/localStorage';
-import { ROLES } from './constants/role';
 
 export default class App {
   async start() {
     const path = window.location.pathname;
-    const role = localStorage.getItem(LOCAL_STORAGE.ROLE);
+    const model = new UserModel();
 
     if (path.includes(PATHS.DASHBOARD)) {
-      if (!role || role.toLowerCase() !== ROLES.SUPER_ADMIN) {
-        window.location.href = PATHS.LOGIN;
-        return;
-      } else {
-        const model = new UserModel();
-        const view = new DashboardView();
-
-        new DashboardController(view, model);
-        return;
-      }
+      const view = new DashboardView();
+      new DashboardController(view, model);
     }
 
-    const model = new UserModel();
     const view = new UserView();
-
     new UserController(view, model);
   }
 }
