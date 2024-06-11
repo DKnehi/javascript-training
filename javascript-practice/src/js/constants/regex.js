206084a1cf2afd63b3cf176d5e2800abc598668d
Truong Quoc Dat <truongquocdatlqd@gmail.com>
2024-06-11 15:56:18 +0700

    update Regex text

--- javascript-practice/src/js/constants/regex.js
+++ javascript-practice/src/js/constants/regex.js
@@ -4,5 +4,5 @@ export const REGEX = {
   //Must be in the correct international phone number format
   PHONE_NUMBER: /^\+(?:[0-9] ?){6,14}[0-9]$/,
   //Cannot contain numbers
-  TEXT: /^[^\d]+$/,
+  TEXT: /^(?!.*\d).+$/,
 };
